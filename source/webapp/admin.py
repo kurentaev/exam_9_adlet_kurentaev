5582d92616c97dc2f4f396aeb3e8eaf9fc9ad647
Adlet Kurentaev <kurentaev@gmail.com>
2022-11-26 12:04:56 +0600

    Добавлена и зарегистрирована в админ панели модель фотографии

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from webapp.models import Photo
 
-# Register your models here.
+admin.site.register(Photo)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    photo = models.ImageField(
+        upload_to='photos',
+        verbose_name='Photo',
+        null=False,
+        blank=False
+    )
+    description = models.CharField(
+        max_length=100,
+        verbose_name='Description',
+        null=False,
+        blank=False
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Created at"
+    )
+    author = models.ForeignKey(
+        to=get_user_model(),
+        verbose_name='Author',
+        related_name='avatars',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.author}"
+
+    class Meta:
+        db_table = "photo"
+        verbose_name = "Photo"
+        verbose_name_plural = "Photos"
+        ordering = ['-created_at', ]
